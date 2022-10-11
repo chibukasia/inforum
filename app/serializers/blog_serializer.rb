923d68ea3a84dad2ebaedd7d009f78b72861b47c
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-11 16:11:17 +0300

    modified blog_serialzer

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,3 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :minutes_to_read, :likes, :created_at
+  attributes :id, :title, :content, :minutes_to_read, :likes, :created_at 
+  belongs_to :user
+  has_many :comments
 end
