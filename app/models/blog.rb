82f55566c65b6117eafc45db571d00cb86197c40
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-11 15:12:51 +0300

    added validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ApplicationRecord
     belongs_to :user 
-    has_many :comments
+    has_many :comments 
+
+    # Validations 
+    validates :title, presence: true 
+    validates :content, presence: true, length: {minimum: 100}
+    validates :minutes_to_read, presence: true
 end
