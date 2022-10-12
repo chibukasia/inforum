220afe53b77eb72cda6e17dcb6712e4bf0559792
oyayafelix <oyayafelix@gmail.com>
2022-10-12 03:15:43 +0300

    added login avi

---
+++ client/src/components/loginform.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import"./loginform.css";
+import loginavi from "../loginavi.png"
+
+const LoginForm = ()=> {
+    return (
+        <div className="loginform">
+            <img src={loginavi} alt="login avi" />
+            
+        </div>
+    )
+}
+
+export default LoginForm
