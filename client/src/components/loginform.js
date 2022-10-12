41c7acc02a09bb4d22667dcfca252074bd7dd01d
oyayafelix <oyayafelix@gmail.com>
2022-10-12 15:45:56 +0300

    nested items in a form and added required syntax for username and password

--- client/src/components/loginform.js
+++ client/src/components/loginform.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import"./loginform.css";
-import loginavi from "../loginavi.png"
+import React from "react";
+import "./loginform.css";
+import loginavi from "../loginavi.png";
+//import {useForm} from "react-hook-form";
 
-const LoginForm = ()=> {
-    return (
-        <div className="loginform">
-            <img src={loginavi} alt="login avi" />
-            <h1>Inforum</h1>
-            <p>Give your ideas a voice.</p>
-            <h2>Log In</h2>
-            <input type="text"placeholder="Username" />
-            <input type="password"placeholder="Password" />
+const LoginForm = () => {
+  return (
+    <div className="loginform">
+        <div className="head">
+      <img src={loginavi} alt="login avi" />
+      <h1>Inforum</h1>
+      <p>Give your ideas a voice.</p>
+      </div>
+      
+      <form>
+        <h2>Log In</h2>
+        <input type="text" placeholder="Username" required="true" /> <br></br> 
+        <input type="password" placeholder="Password" required="true" /><br></br>
 
-            <div>
-            <button type="button" className="login-btn"> <h3>Continue</h3></button>
-            </div>
-            <h3>Don't have an account?</h3>
-            <a href="./loginform.js">Sign Up</a>
+        <div>
+          <input className="login-btn" type="submit" value="Login" />
         </div>
-    )
-}
+        <h3>Don't have an account?</h3>
+        <a href="./loginform.js">Sign Up</a>
+      </form>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
