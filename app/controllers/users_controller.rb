3df334fde4f292445f1d0cc7e53a463c51a7db3d
Kelvin <vokemalosh32@gmail.com>
2022-10-12 13:32:37 +0300

    refactor to make code dry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,6 @@ class UsersController < ApplicationController
         if user.valid?
             session[:user_id] = user.id
             render json: user, status: :created
-        else
-            render json: { errors: user.errors.full_messages }
         end
     end
 
