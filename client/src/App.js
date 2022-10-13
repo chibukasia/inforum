f9e5c3d148f8a11274f313ccf6b4b6a115e7c9fa
chibukasia <39667302+chibukasia@users.noreply.github.com>
2022-10-13 19:23:33 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,20 @@
 import './App.css';
 import LoginForm from './components/login';
 import {Routes, Route, Link} from "react-router-dom";
-import React, {useState} from 'react';
+import Blogs from './components/blogs';
+import Blog from './components/blog';
+import { useEffect, useState } from 'react';
+import AddBlog from './components/addblog';
+
 function App() {
-  // const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]) 
+  const [search, setSearch] = useState('')
+
+  useEffect(()=>{
+    fetch("/blogs")
+    .then(res=>res.json())
+    .then(data=>setBlogs(data))
+  }, [])
 
   return (
     <div className='main'>
