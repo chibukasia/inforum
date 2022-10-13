2d75721cc34a9821929ad1c83712d6d65301db29
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-13 21:51:47 +0300

    added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -18,17 +18,25 @@ function App() {
     .then(data=>setBlogs(data))
   }, [])
   return (
-    <div className='main'>
+    <div>
       <nav>
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/blogs" className="nav-item">Blogs</Link>
+        <Link to="/addblog" className="nav-item">Write</Link>
+        <Link to="/signup" className="nav-item">Sign Up</Link>
+        <Link to="/login" className="nav-item">Login</Link>
       </nav>
+    <div className='main'>
       <Routes>
         <Route exact path='/login' element={<LoginForm/>}/>
         <Route exact path='/blogs' element={<Blogs blogs={blogs} setSearch={setSearch} search={search}/>}/>
         <Route exact path='/addblog' element={<AddBlog setBlogs={setBlogs}/>}/>
-        <Route exact path='/:id' element={<Blog blogs={blogs}/>}/>
+        <Route exact path='/blogs/:id' element={<Blog blogs={blogs}/>}/>
         <Route exact path='/signup' element={<SignUp/>}/>
       </Routes>
     </div>
+    </div>
+      
   );
 }
 
