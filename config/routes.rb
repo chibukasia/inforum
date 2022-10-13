8f2bf42b823fba38787ac8c3449bdf0d81b98bba
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-13 21:54:34 +0300

    added index on comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :blogs
-  resources :comments, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
