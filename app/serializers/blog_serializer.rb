75a067b0dc5b82ba07f767f26322ffeca53b1faa
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-13 10:11:07 +0300

    added summary

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,9 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :minutes_to_read, :likes, :created_at 
+  attributes :id, :title, :content, :minutes_to_read, :likes, :created_at, :summary
   belongs_to :user
-  has_many :comments
+  has_many :comments 
+
+  def summary 
+    "#{self.object.content[0..150]...}"
+  end
 end
